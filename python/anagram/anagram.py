12ab503011600822332378e8c4c20ecb6de63ee1
Ishu Kalra <ishukalraish2@gmail.com>
2017-12-27 21:52:51 +0530

    Update anagram.py

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -1,2 +1,21 @@
 def detect_anagrams(word, candidates):
-    pass
+    viable_candidates = []
+    length = len(word)
+    temp_word = word.lower()
+    for candidate in candidates:
+        candidate_case_preserve = candidate
+        candidate = candidate.lower()
+        word = temp_word
+        if len(word) != len(candidate) or word == candidate:
+            continue
+        for char in candidate:
+            if word.find(char) != -1:
+                pos = word.find(char)
+                word = word[0:pos] + word[pos+1:length]
+            else: break
+        if word == '':
+            viable_candidates.append(candidate_case_preserve)
+    return viable_candidates
+
+str1 = 'corn'
+print(detect_anagrams('corn', ["corn", "dark", "Corn", "rank", "CORN", "cron", "park"]))
